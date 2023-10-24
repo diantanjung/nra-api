0dc24210383fcb16dfdcd08c18bc6941f7835a8c
diantanjung <dianmulyanat@gmail.com>
2023-10-24 14:37:49 +0700

    skip upload digital ocean

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -18,14 +18,16 @@ class UploadController extends Controller
      */
     public function store(Request $request): JsonResponse
     {
-        $this->validate($request, [
-            'file' => 'required|file|mimes:pdf,doc,docx,jpeg,png,jpg|max:5120',
-        ]);
+        // $this->validate($request, [
+        //     'file' => 'required|file|mimes:pdf,doc,docx,jpeg,png,jpg|max:5120',
+        // ]);
 
-        $file_name = randomString(20) . '_' . time() . '.' . $request->file->extension();
-        Storage::disk('do')->put($file_name, file_get_contents($request->file));
+        // $file_name = randomString(20) . '_' . time() . '.' . $request->file->extension();
+        // Storage::disk('do')->put($file_name, file_get_contents($request->file));
 
-        $cdn_url = config('filesystems.disks.do.cdn_endpoint') . "/" . $file_name;
-        return responseSuccess(compact('cdn_url'));
+        // $cdn_url = config('filesystems.disks.do.cdn_endpoint') . "/" . $file_name;
+        // return responseSuccess(compact('cdn_url'));
+
+        return responseSuccess("https://ui-avatars.com/api/?name=WR&background=random");
     }
 }
